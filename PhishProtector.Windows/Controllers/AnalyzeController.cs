2c1319e4fef4aeb248ee2d468114ced5b87e562d
Akutsu <Akutsu@AKUTSU-PC>
2023-04-15 17:40:14 +0200

    Comparaison d'url

--- PhishProtector.Windows/Controllers/AnalyzeController.cs
+++ PhishProtector.Windows/Controllers/AnalyzeController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using PhishAnalyzer;
 using System.Globalization;
+using System.Security.Policy;
 
 namespace PhishProtector.Windows.Controllers
 {
@@ -11,17 +12,19 @@ namespace PhishProtector.Windows.Controllers
     public class AnalyzeController : ControllerBase
     {
         [HttpPost]
-      //  public IActionResult Post([FromForm] string url, [FromForm] byte[] screenBytes)
+        //  public IActionResult Post([FromForm] string url, [FromForm] byte[] screenBytes)
         public IActionResult Post([FromForm] string url, [FromForm] string screenBytes)
         {
-            try { 
-            if(!string.IsNullOrEmpty(url)   && screenBytes != null) {
+            try
+            {
+                if (!string.IsNullOrEmpty(url) && screenBytes != null)
+                {
                     int base64StartIndex = screenBytes.IndexOf(',') + 1;
                     string base64String = screenBytes.Substring(base64StartIndex);
 
                     byte[] imageByteArray = Convert.FromBase64String(base64String);
- 
- 
+
+
                     SiteClassification.ModelInput sampleData = new SiteClassification.ModelInput()
                     {
                         ImageSource = imageByteArray,
@@ -29,13 +32,36 @@ namespace PhishProtector.Windows.Controllers
 
                     // Load model and predict output
                     var result = SiteClassificationManager.Predict(sampleData);
+
+
+                    if (result != null  )
+                    {
+ 
+                        if (UriComparaison.IsSameOfficialSite(url, result.PredictedLabel)) {
+                            //Le site  ressemble à un site sous surveillance et l'url est bonne
+                            return Ok(true);
+                        }
+                        else {
+                            //Le site  ressemble à un site sous surveillance mais l'url n'est pas la même
+                            //Ameliorer ce système pour prendre en compte les filliales....
+                            return Ok(false);
+                        }
+
+
+                    }
+                    else { 
+                        //Le site ne ressemble à aucun site sous surveillance donc c'est bon
+                        return Ok(true);
+                    }
                 }
-            return Ok();
+                //L'url ou le screen du site est vie impossible de traiter le site
+                return BadRequest("Url and/or Screen are empty");
             }
             catch (Exception ex)
             {
-                return Ok();
-            }
+                //Erreur interne
+                return StatusCode(500, ex.Message); 
+             }
         }
 
     }
